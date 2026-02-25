1fde960ea2288dac267a6e56c3f299308b839d3b
李悠ジェ <yuje0109@icloud.com>
2026-02-25 19:56:18 +0900

    サンプルスクリプト修正

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -5,7 +5,7 @@ public class Sample : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("スタート");
     }
 
     // Update is called once per frame
