16454072b8a78ac836b78018d24f7e2b34513311
李悠ジェ <yuje0109@icloud.com>
2026-03-04 15:59:37 +0900

    EnemyController修正

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System.Linq;　　　//　<=　宣言を追加
+
 
 public class EnemyController : MonoBehaviour
 {
@@ -11,18 +13,12 @@ public class EnemyController : MonoBehaviour
     [SerializeField, Header("移動速度")]
     private float moveSpeed;
 
-    private Vector3[] paths;     // 移動する各地点を代入するための配列
+    private Vector3[] paths;
 
     void Start()
     {
-        // 移動する地点を取得するための配列の初期化
-        paths = new Vector3[pathData.pathTranArray.Length];
-
-        // 移動する位置情報を順番に配列に取得
-        for (int i = 0; i < pathData.pathTranArray.Length; i++)
-        {
-            paths[i] = pathData.pathTranArray[i].position;
-        }
+        // 移動する地点を取得
+        paths = pathData.pathTranArray.Select(x => x.position).ToArray();
 
         // 各地点に向けて移動
         transform.DOPath(paths, 1000 / moveSpeed).SetEase(Ease.Linear);
