26230e14a700c60ad105c0d4a3a16f72b7f0a119
李悠ジェ <yuje0109@icloud.com>
2026-03-18 03:22:19 +0900

    DrawPathLine実装

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;　　　　　//　<=　☆　追加します
 
 public class EnemyGenerator : MonoBehaviour
 {
@@ -10,46 +11,34 @@ public class EnemyGenerator : MonoBehaviour
     [SerializeField]
     private PathData pathData;
 
-    private GameManager gameManager;   //　☆　追加
+    [SerializeField]
+    private DrawPathLine pathLinePrefab;
 
+    private GameManager gameManager;
 
-    //public bool isEnemyGenerate;　　　　//　☆　以下４つの変数を削除
-
-    //public int generateIntervalTime;
-
-    //public int generateEnemyCount;
-
-    //public int maxEnemyCount;
-
-
-    //void Start() {
-    //isEnemyGenerate = true;
-
-    //StartCoroutine(PreparateEnemyGenerate());    
-    //}
 
     /// <summary>
     /// 敵の生成準備
     /// </summary>
     /// <returns></returns>
     public IEnumerator PreparateEnemyGenerate(GameManager gameManager)
-    {　　　//　☆①　引数を追加します
+    {
 
-        this.gameManager = gameManager;             //　☆②　処理を追加します
+        this.gameManager = gameManager;
 
         // 生成用のタイマー用意
         int timer = 0;
 
         // isEnemyGenetate が true の間はループする
         while (gameManager.isEnemyGenerate)
-        {                  //　☆③　条件を変更します
+        {
 
             // タイマーを加算
             timer++;
 
             // タイマーの値が敵の生成待機時間を超えたら
             if (timer > gameManager.generateIntervalTime)
-            {    //  ☆④　条件の右辺を変更します
+            {
 
                 // 次の生成のためにタイマーをリセット
                 timer = 0;
@@ -58,20 +47,10 @@ public class EnemyGenerator : MonoBehaviour
                 GenerateEnemy();
 
                 // 敵の生成数のカウントアップと List への追加
-                gameManager.AddEnemyList();　　　　　　　//　☆追加
+                gameManager.AddEnemyList();
 
                 // 最大生成数を超えたら生成停止
-                gameManager.JudgeGenerateEnemysEnd();  //　☆追加
-
-                // 生成した数をカウントアップ
-                //generateEnemyCount++;
-
-                // 敵の最大生成数を超えたら
-                //if (generateEnemyCount >= maxEnemyCount) {
-
-                // 生成停止
-                //isEnemyGenerate = false;
-                //}
+                gameManager.JudgeGenerateEnemysEnd();
             }
 
             // 1フレーム中断
@@ -91,14 +70,63 @@ public class EnemyGenerator : MonoBehaviour
         // 指定した位置に敵を生成
         EnemyController enemyController = Instantiate(enemyControllerPrefab, pathData.generateTran.position, Quaternion.identity);
 
+        // 移動する地点を取得(<=　いままでEnemyController スクリプト内で行っていた処理をこちらに移動します)
+        Vector3[] paths = pathData.pathTranArray.Select(x => x.position).ToArray();
 
-        // TODO 移動する地点を取得
+        // 敵キャラの初期設定を行い、移動を一時停止しておく
+        enemyController.SetUpEnemyController(paths);
 
+        // 敵の移動経路のライン表示を生成の準備
+        StartCoroutine(PreparateCreatePathLine(paths, enemyController));
+    }
 
-        // TODO 敵キャラの初期設定を行い、移動を一時停止しておく
+    /// <summary>
+    /// ライン生成の準備
+    /// </summary>
+    /// <param name="paths"></param>
+    /// <returns></returns>
+    private IEnumerator PreparateCreatePathLine(Vector3[] paths, EnemyController enemyController)
+    {
 
+        // ラインの生成と削除。この処理が終了するまでは、この処理より下の処理は実行されない
+        yield return StartCoroutine(CreatePathLine(paths));
 
-        // TODO 敵の移動経路のライン表示を生成の準備
+        // 敵の移動を再開
+        enemyController.ResumeMove();
+    }
 
+    /// <summary>
+    /// 移動経路用のラインの生成と破棄
+    /// </summary>
+    private IEnumerator CreatePathLine(Vector3[] paths)
+    {
+
+        // List の宣言と初期化
+        List<DrawPathLine> drawPathLinesList = new List<DrawPathLine>();
+
+        // １つの Path ごとに１つずつ順番にラインを生成
+        for (int i = 0; i < paths.Length - 1; i++)
+        {
+            DrawPathLine drawPathLine = Instantiate(pathLinePrefab, transform.position, Quaternion.identity);
+
+            Vector3[] drawPaths = new Vector3[2] { paths[i], paths[i + 1] };
+
+            drawPathLine.CreatePathLine(drawPaths);
+
+            drawPathLinesList.Add(drawPathLine);
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        // すべてのラインを生成して待機
+        yield return new WaitForSeconds(0.5f);
+
+        // １つのラインずつ順番に削除する
+        for (int i = 0; i < drawPathLinesList.Count; i++)
+        {
+            Destroy(drawPathLinesList[i].gameObject);
+
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
